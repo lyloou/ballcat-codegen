9b735daff4f16035726d4264cb03c4c0cc484656
lilou <lilou@coocaa.com>
2021-09-06 10:43:02 +0800

    :wrench: 更新 publish-to-beta.sh

--- publish-to-beta.sh
+++ publish-to-beta.sh
@@ -4,6 +4,6 @@ rm -rf ./target-jar/
 mkdir -p ./target-jar/
 
 cp -f ballcat-codegen-backend/target/ballcat-codegen-backend.jar ./target-jar/ballcat-codegen-backend.jar
-scp ./target-jar/ballcat-codegen-backend.jar root@106.75.185.163://root/w/compose-hub/context/ballcat-codegen/app.jar
+scp ./target-jar/ballcat-codegen-backend.jar root@ballcat.host://root/w/compose-hub/context/ballcat-codegen/app.jar
 
-ssh root@106.75.185.163 "pwd;cd /root/w/compose-hub;sh /root/w/compose-hub/deploy/deploy_ballcat_codegen.sh"
+ssh root@ballcat.host "pwd;cd /root/w/compose-hub;sh /root/w/compose-hub/deploy/deploy_ballcat_codegen.sh"
